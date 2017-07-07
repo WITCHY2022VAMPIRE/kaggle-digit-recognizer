ab83b481494ae7b5669710fa2d0a857ac851b395
Sky <skyhuang1208@users.noreply.github.com>
2017-07-07 14:53:33 -0700

    v1.0.4 modify read_data

--- codes/read_data.py
+++ codes/read_data.py
@@ -4,11 +4,20 @@ import numpy as np
 def read():
     x_size = 784
     
-    print('Reading training data file')
+    print('### Reading training data file...')
 
-    train_data_set = np.array([])
-        
-    with open('../data/train.csv') as train_f:
-        for line in train_f:
-            pass
-    return (1,2)
+    feature_train= []
+    label_train= []
+    feature_test= []
+    label_test= []
+    with open('../data/train.csv') as IFILE_TRAIN:
+        for i, line in enumerate(IFILE_TRAIN):
+            s= line.strip().split(',')
+            feature_train.append(s[1:])
+            label_train.append(s[0])
+    with open('../data/test.csv') as IFILE_TEST:
+        for i, line in enumerate(IFILE_TEST):
+            s= line.strip().split(',')
+            feature_test.append(s[1:])
+            label_test.append(s[0])
+    return feature_train, label_train, feature_test, label_test 
