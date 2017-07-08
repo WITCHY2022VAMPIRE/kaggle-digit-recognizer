99b9ce32b8e9a4c01e62700d1d0cc482b7caae4a
louis925 <Louis.Yang.925@gmail.com>
2017-07-07 17:37:27 -0700

    v1.2 create neural_network.py

--- codes/digit_recognizer.py
+++ codes/digit_recognizer.py
@@ -1,8 +1,11 @@
 import read_data as rd
 import plot_digit as pd
+import neural_network as nn
 
 length_x = 28
 length_y = 28
+rescale_base = 255
+test_split = 1000
 
 def main():
     print('Kaggle Digit Recognizer')
@@ -14,9 +17,14 @@ def main():
     print(train_labels.shape)
     print(train_features[1,])
     print(train_labels[1])
+    print(test_features.shape)
+    print(test_labels.shape)
     
-    pd.display(train_features[1], length_x, length_y)
-    
+    #pd.display(train_features[1], length_x, length_y)
+
+    n_train = len(train_labels)
+    trained_nn = nn.train_neural_network(train_features[:(n_train - test_split)], train_labels[:(n_train - test_split)], rescale_base)
+    nn.test_neural_network(trained_nn, train_features[(n_train - test_split):n_train], train_labels[(n_train - test_split):n_train], rescale_base)
 
 if __name__ == '__main__':
     main()
---
+++ codes/neural_network.py
@@ -0,0 +1,35 @@
+import numpy as np
+#from sklearn.preprocessing import StandardScaler # scaling data
+from sklearn.neural_network import MLPClassifier # neural network
+
+def rescale(data_features, max_value, min_value = 0.0):
+    return (data_features - min_value) / (max_value - min_value)
+
+def train_neural_network(train_features, train_labels, rescale_base):
+    # Rescale data
+    print('Rescaling training data...')
+    train_features_s = rescale(train_features, rescale_base)
+    
+    nn = MLPClassifier(hidden_layer_sizes = (100, 100), max_iter = 400)
+    print('Training neural network...')
+    nn.fit(train_features_s, train_labels)
+    print('Lost: ', nn.loss_)
+    print('Train score: ', nn.score(train_features_s, train_labels))
+    return nn
+
+def test_neural_network(trained_neural_network, test_features, test_labels, rescale_base):
+    # Rescale data
+    print('Rescaling test data...')
+    test_features_s = rescale(test_features, rescale_base)
+
+    print('Sklearn test score: ', trained_neural_network.score(test_features_s, test_labels))
+
+    test_predict = trained_neural_network.predict(test_features_s)
+    n_correct = np.sum(test_predict == test_labels)
+    n_total = len(test_labels)
+    print('Total test number: ', n_total)
+    print('Correct test cases: ', n_correct)
+    print('Accuracy: ', n_correct / n_total )
+    
+    return test_predict
+    
